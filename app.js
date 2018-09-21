4e3c711382ed9117dac09a7d7059b871d6af4d47
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-09-21 16:35:43 +1000

    Basic mongo app.js setup.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
 const url = 'mongodb://localhost:27017';
-const dbName = 'mydb';
-
 // Use connect method to connect to the server
 MongoClient.connect(url, {poolSize:10}, function(err, client) {
     if (err) { return console.log(err) }
+    const dbName = 'mydb';
+    const db = client.db(dbName);
 });
