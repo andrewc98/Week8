c4d8eae29fb7b06cd72270d309a7d66edbd32c02
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-09-21 18:05:43 +1000

    Completed.

--- app.js
+++ app.js
@@ -17,56 +17,29 @@ app.get('/home',function(request,response){
         const dbName = 'mydb';
         var products = db.db(dbName);
     
-        // Drop products
-        products.collection("products").drop(function(err, res) {
-            if (err) { return console.log(err) }
-            console.log(res);
-        });
-        // Drop products
-    
-        // Create products
-        products.createCollection("products", function(err, res) {
-            if (err) { return console.log(err) }
-            console.log("Created");
-        });
-        // Create products
+        // Drop & Create products
+        const create = require('./create.js');
+        create.createDatabase(products);
+        // Drop & Create products
     
         // Insert products
-        const products_to_add = [
-            { _id: 1, name: 'Soap', price: 1.5, type: 'Toiletries', description: 'Smells of roses' },
-            { _id: 2, name: 'Toothpaste', price: 3.99, type: 'Toiletries', description: 'Teeth Whitening' },
-            { _id: 3, name: 'Battery Pack', price: 12.99, type: 'Hardware', description: '12 Batteries per pack' }
-        ];
-        products.collection("products").insertMany(products_to_add, function(err, res) {
-            if (err) { return console.log(err) }
-            console.log("Inserted: " + res.insertedCount);
-        });
+        const insert = require('./add.js');
+        insert.addProduct(products);
         // Insert products
     
         // Delete a product
-        const one_product = { _id: 1, name: 'Soap', price: 1.5, type: 'Toiletries', description: 'Smells of roses' };
-        products.collection("products").deleteOne(one_product, function(err, obj) {
-            if (err) { return console.log(err) }
-            console.log("Deleted: " + one_product.name);
-        });
+        const remove = require('./remove.js');
+        remove.deleteProduct(products);
         // Delete a product
     
         // Update a product
-        const product_to_update = { _id: 2 };
-        var update_product = { $set: {name: "Mouthwash", description: "Cleans all the nooks and crannies" } };
-        products.collection("products").updateOne(product_to_update, update_product, function(err, res) {
-            if (err) { return console.log(err) }
-            console.log("Updated");
-        });
+        const update = require('./update.js');
+        update.updateProduct(products);
         // Update a product
     
         // Find products
-        products.collection("products").find({}).toArray(function(err, result) {
-            if (err) { return console.log(err) }
-            console.log(result);
-            db.close();
-        });
+        const find = require('./read.js');
+        find.readProducts(products, db);
         // Find products
     });
-    // response.sendFile(__dirname + "/www/index.html");
 });
--- read.js
+++ read.js
@@ -1,3 +1,9 @@
-module.exports = function(app, db){
-    app.post('/api/aith')
+module.exports = {
+    readProducts(products, db) {
+        products.collection("products").find({}).toArray(function(err, result) {
+            if (err) { return console.log(err) }
+            console.log(result);
+            db.close();
+        });
+    }
 }
